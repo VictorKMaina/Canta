d94563072466c6e33ec6edc16f461b75358c3511
fredrick-kyalo <kyalomwendwa44@gmail.com>
2020-03-03 17:13:52 +0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    $("#dashboard").click(function () {
+        $(".dash").slideDown();
+
+        $(".one").click(function() {
+            $("#userequests").toggle();
+        });
+        $(".prog").click(function() {
+            $("#progress").toggle();
+        });
+        $(".three").click(function(){
+        $("#usereviews").toggle();
+        
+    });
+
+    });
+});
